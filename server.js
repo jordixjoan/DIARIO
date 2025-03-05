158ffc4007c831cbe5655f5a04ad5dad2b6288c2
jordixjoan <jordijoan_98@hotmail.com>
2025-03-05 15:47:10 +0100

    15

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.post("/create-checkout-session", async (req, res) => {
     try {
         const { items } = req.body;
-
+        console.log("hola");
         const lineItems = items.map(item => ({
             price_data: {
                 currency: "eur",
