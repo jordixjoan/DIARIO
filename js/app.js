38bb7fc2ff884d54daf9978610c2432d73782ac4
David Español <despanol@pacifico-meetings.com>
2025-03-06 21:15:26 +0100

    David Update API URL

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ async function procesarPago () {
     try {
 
 
-        const response = await fetch("http://127.0.0.1:5000/create-checkout-session", {
+        const response = await fetch("https://diario-production-1.up.railway.app/create-checkout-session", {
             method: "POST",
 
             headers: { "Content-Type": "application/json" },
