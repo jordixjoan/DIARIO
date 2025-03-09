afd356f35ff27a83702c13adaeecb8d1a6c9744b
jordixjoan <jordijoan_98@hotmail.com>
2025-03-09 20:42:27 +0100

    ok

--- server.js
+++ server.js
@@ -95,5 +95,5 @@ app.post("/guardar-correo", async (req, res) => {
 
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
