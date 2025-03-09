c08f3efe9b4ea6ec841219d0631aee7e7c1bee5a
jordixjoan <jordijoan_98@hotmail.com>
2025-03-09 20:05:26 +0100

    19

--- server.js
+++ server.js
@@ -69,4 +69,4 @@ app.post("/create-checkout-session", async (req, res) => {
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
